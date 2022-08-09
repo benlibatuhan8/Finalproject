ec2e48cf93c58bdded8a7b27e022cdc728eac482
Batuhan Benli <benlibatuhan6@gmail.com>
2022-08-09 12:23:49 +0300

    Dto

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,7 @@
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,6 +23,21 @@ namespace Business.Concrete
             // iş kodları   
             return _productDal.GetAll();
 
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
         }
     }
 }
